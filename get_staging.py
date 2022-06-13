9d1cb72f6672d407a2d42b293e5f1a8afe77fea1
Frank A <fr.alvarez1@outlook.com>
2022-06-13 14:23:54 -0400

    Created arg_parser and initial logic

--- get_staging.py
+++ get_staging.py
@@ -2,9 +2,9 @@
 
 # Flow
 
-import os   # to expose command line tools
-
-# Function: query_dns
+import os   # to expose command line functions
+import sys, getopt
+# Function: query_dns()
 #   * dig command:  get $hostname CNAME *.edgekey.net
 #   * store dig output
 #   * parse output:  get edgekey.net CNAME *.akamaiedge.net
@@ -12,9 +12,63 @@ import os   # to expose command line tools
 #   * dig command: get *.akamaiedge-staging.net A ${staging-ip}
 #   * return ${staging-ip}
 
-def query_dns():
-    cmd = os.system("dig www.google.com")
+# Function is_admin()
+
+def usage():
+    print("options:")
+    print("-h     Print this Help menu.")
+    print("-v     Verbose mode.")
+    print("     Hostname (i.e: www.google.com).")
+    print("-f     File to read from.")
+    print("-o     File to output to")
+
+
+
+
+def query_dns(hostname) -> str:
+
+    hostname = hostname
+    # First dns query
+    dig_string = f"dig +noadditional +noquestion +nocomments +nocmd +nostats {hostname}" + " | awk '{print $1, $4, $5, \"\\n\"}'"
+
+    #cmd_string = dig_string + awk_string 
+    cmd = os.system(dig_string)   # Execute
+    
     print(cmd)
 
 
-query_dns()
+def main():
+    outputFile = "" # expect String
+    filePath = ""  # expect String
+    verbose = False  # expect Bool
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hvo:f:", ["help", "verbose", "output", "file"])
+    except getopt.GetoptError as err:
+        #usage()
+        sys.exit()
+    for opt, arg in opts:
+        # Help Menu opt
+        if opt == "-h" or opt == "--help":
+            usage()
+        # Verbose opt
+        elif opt == "-v" or opt =="--verbose":
+            verbose = True
+            print("verbose")
+        # Output file opt
+        elif opt == "-o" or opt == "--output":
+            outputFile = arg
+            print(f"This is the outputFile: {outputFile}")
+        # Input file opt
+        elif opt == "-f" or opt == "--file":
+            filePath = arg
+            print(f"This is the input file path: {filePath}")
+        else:
+            assert False, "unhandled option"
+        
+if __name__ == '__main__':
+    main()
+
+
+def main():
+    
