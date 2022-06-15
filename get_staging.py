187f808b8fc6f6d193be444f04f6abd1a631988e
Frank A <fr.alvarez1@outlook.com>
2022-06-15 11:03:45 -0400

    Added logic
    Updated query_dns to query_dns_init which contains main logic for the app.
    Will be splitting the logic into smaller functions as progress continues
    
    query_dns() will return ONLY the dns_records list.

--- get_staging.py
+++ get_staging.py
@@ -1,20 +1,21 @@
 #!/var/bin/python3
 
 # Flow
-
-import os   # to expose command line functions
-import sys, getopt
-# Function: query_dns()
-#   * dig command:  get $hostname CNAME *.edgekey.net
-#   * store dig output
-#   * parse output:  get edgekey.net CNAME *.akamaiedge.net
-#   * add string: "-staging" to ${*.akamaiedge.net} --> *.akamaiedge-staging.net
-#   * dig command: get *.akamaiedge-staging.net A ${staging-ip}
-#   * return ${staging-ip}
-
 # Function is_admin()
 
+import sys, getopt, subprocess
+
+
+# Initialize global variables
+outputFile = "" # expect String
+filePath = ""  # expect String
+verbose = False  # expect Bool
+hostname = "" # expect String
+
+# Help Menu
 def usage():
+
+    print("./get_staging.py www.google.com -f /tmp/hosts -o /tmp/output")
     print("options:")
     print("-h     Print this Help menu.")
     print("-v     Verbose mode.")
@@ -23,31 +24,93 @@ def usage():
     print("-o     File to output to")
 
 
-
+# Function: query_dns()
+    #   * dig command:  get $hostname CNAME *.edgekey.net
+    #   * store dig output
+    #   * parse output:  get edgekey.net CNAME *.akamaiedge.net
+    #   * add string: "-staging" to ${*.akamaiedge.net} --> *.akamaiedge-staging.net
+    #   * dig command: get *.akamaiedge-staging.net A ${staging-ip}
+    #   * return ${staging-ip}
 
 def query_dns(hostname) -> str:
+    host = str(hostname)
 
-    hostname = hostname
     # First dns query
-    dig_string = f"dig +noadditional +noquestion +nocomments +nocmd +nostats {hostname}" + " | awk '{print $1, $4, $5, \"\\n\"}'"
+    dig_string = "/usr/bin/dig +noadditional +noquestion +nocomments +nocmd +nostats " + host
+    # Split into list for subprocess
+    p_args = dig_string.split(' ')
+    # Create Subprocess to execute commands.
+    proc = subprocess.Popen(p_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    # First query results
+    out = proc.stdout.read().replace("\t", " ")#.decode("utf-8").rstrip()
+    # Creates list of each dns record in query
+    split = out.splitlines()
+    dns_records = [line.split() for line in split]
+    print(dns_records)
 
-    #cmd_string = dig_string + awk_string 
-    cmd = os.system(dig_string)   # Execute
+
+def records_parse(hostname) -> str:
     
-    print(cmd)
+    # Define variables
+    host = str(hostname)
+    edge_hostname = ""
 
+    # First dns query
+    dig_string = "/usr/bin/dig +noadditional +noquestion +nocomments +nocmd +nostats " + host
+    # Split into list for subprocess
+    p_args = dig_string.split(' ')
+    # Create Subprocess to execute commands.
+    proc = subprocess.Popen(p_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    # First query results
+    out = proc.stdout.read().replace("\t", " ")#.decode("utf-8").rstrip()
+    
+    # Creates list of lines
+    split = out.splitlines()
+    dns_records = [line.split() for line in split]
+    print(dns_records)
+
+
+    ###### Checks first dns request for hostname to edge_hostname  ######
+    print("\nStage 1: \n")
+    for record in dns_records:
+        # If hostname matches AND the record is cname AND it points to an edgekey / edgesuite host
+        if (record[0] == hostname + '.') and (record[3] == "CNAME") and ("edgekey" or "edgesuite" in record[4]):
+            print("Hostname validated: " + record[0])
+            print("CNAME validated: " + record[4])
+            edge_hostname = record[4]
+        #except:
+        #       print("This hostname is not CNAME'd to akamai")
+    
+    ###### Stage 2 check will search for edge_hostname to edge server ######
+    print("\nStage 2: \n")
+    for record in dns_records:
+        if (record[0] == edge_hostname) and (record[3] == "CNAME") and ("akamaiedge" or "akadns" in record[4]):
+            print("Edge Hostname validated: " + edge_hostname)
+            print("Edge Server identified: " + record[4])
+            edge_server = record[4]
+            # If akadns, we must repeat one more time to find the edge server
+            #maybe recurse to take care of this additional factor
+            if ("akadns" in edge_server):
+                print("\nThis is a Akamai Global Traffic Manager hostname")
+                print("Needs an additional iteration to reach edge server")
+                continue
+            if ("akamaiedge" in edge_server):
+                print("Success! This is an edge server")
+                continue
+            
+    ###### Stage 3 will lookup the A record for edge server and return IP address
+    
+    
 
 def get_opt():
-    outputFile = "" # expect String
-    filePath = ""  # expect String
-    verbose = False  # expect Bool
 
     try:
         opt_list, args = getopt.getopt(sys.argv[1:], "hvo:f:", ["help", "verbose", "output=", "file="])
     except getopt.GetoptError as err:
         usage()
-        #sys.exit(2)
-        pass
+        sys.exit(2)
+        
+    
     for opt, arg in opt_list:
         # Help Menu opt
         if opt in ("-h", "--help"):
@@ -69,10 +132,16 @@ def get_opt():
         else:
             assert False, "unhandled option"
 
+    if len(args) == 1:
+        hostname = args[0]
+        return hostname
+    else:
+        usage()
+        
+
 def main():
-    get_opt()
-    
+    hostname = get_opt()
+    records_parse(hostname)
         
 if __name__ == '__main__':
     main()
-
